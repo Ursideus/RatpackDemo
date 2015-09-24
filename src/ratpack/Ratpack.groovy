3a433721f57440fa4b8d6ec404e24d5b02c1a37e
Waldman <Waldman>
2015-09-24 16:03:57 +0300

    Add ConfigData load

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,3 +1,4 @@
+import ratpack.config.ConfigData
 import ratpack.groovy.template.MarkupTemplateModule
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
@@ -6,9 +7,25 @@ import static ratpack.groovy.Groovy.ratpack
 ratpack {
   bindings {
     module MarkupTemplateModule
+    // Define config file/Env settings path
+    ConfigData configData = ConfigData.of()
+            .yaml(ClassLoader.getSystemResource('application.yml'))
+            .props(ClassLoader.getSystemResource('application.properties') as URL)
+            .sysProps()
+            .env()
+            .build()
+    bindInstance(ConfigData, configData) //this loads this instance into Guice
   }
 
   handlers {
+    // Read config file/Env settings
+    handler { ConfigData configData ->
+      println configData.get("/property1", String)
+      println configData.get("/property2/subproperty2", String)
+      println configData.get("/javasysprop", String)
+      println configData.get("/MYPROP", String)
+      render "done"
+    }
     get {
       render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
     }
