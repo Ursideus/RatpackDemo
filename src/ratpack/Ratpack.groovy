5d18208b7f78c980591282c6ebf1f64ea4a37035
Tornaq <Tornaq>
2015-09-28 10:38:30 +0300

    Add json habdler

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -4,7 +4,30 @@ import static ratpack.groovy.Groovy.ratpack
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import ratpack.groovy.template.MarkupTemplateModule
 
+import ratpack.jackson.guice.JacksonModule
+import static ratpack.jackson.Jackson.json
+
 ratpack {
+    bindings {
+        module JacksonModule
+    }
+    handlers {
+        get {
+            render 'Hello Bluemix!'
+        }
+        get('person') {
+            render json(new Person(id: 1, name: 'John Doe', age: 31))
+        }
+    }
+}
+
+class Person {
+   Long id
+   String name
+   int age
+}
+
+/*ratpack {
     handlers {
         get('users/:id') { //ex: /users/xyz123
             render context.pathTokens['id']
@@ -17,8 +40,8 @@ ratpack {
             render context.pathTokens['string'] * context.pathTokens['times'].toInteger()
         }
     }
-}
-
+}*/
+  
 /*ratpack {
   handlers {
     get('getmapping') {
